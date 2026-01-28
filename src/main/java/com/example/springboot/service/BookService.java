ddb46810538ad853ce3d890ae28aae28e3366e2e
noleynik29 <71104368+noleynik29@users.noreply.github.com>
2026-01-28 12:41:07 +0200

    Delete src/main/java/com/example/springboot/service directory

--- src/main/java/com/example/springboot/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.service;
-
-import com.example.springboot.entity.Book;
-import java.util.List;
-
-public interface BookService {
-    Book save(Book book);
-    List<Book> findAll();
-}
