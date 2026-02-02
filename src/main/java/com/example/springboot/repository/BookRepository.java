5418b4f899fa26272888559a41f8aa354ac0b06f
noleynik29 <noleynik29@gmail.com>
2026-02-02 15:03:00 +0200

    criteria query homework

--- src/main/java/com/example/springboot/repository/BookRepository.java
+++ src/main/java/com/example/springboot/repository/BookRepository.java
@@ -1,8 +1,8 @@
 package com.example.springboot.repository;
 
 import com.example.springboot.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     List<Book> search(String title, String author, String isbn);
