f1aa0bb4c8d3c88273619b5d37cf19a4c4ffee2f
noleynik29 <noleynik29@gmail.com>
2026-02-04 13:19:12 +0200

    validation and globalexceptionhandler homework

--- src/main/java/com/example/springboot/dto/BookDto.java
+++ src/main/java/com/example/springboot/dto/BookDto.java
@@ -1,8 +1,8 @@
 package com.example.springboot.dto;
 
-import java.math.BigDecimal;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
