9ca6450267f7e730105fc3a8db58d6030edf70c6
noleynik29 <noleynik29@gmail.com>
2026-02-18 14:55:06 +0200

    spring boot category

--- src/main/java/com/example/springboot/entity/Book.java
+++ src/main/java/com/example/springboot/entity/Book.java
@@ -14,11 +14,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
 @Setter
