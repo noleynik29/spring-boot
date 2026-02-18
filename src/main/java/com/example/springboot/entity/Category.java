e0e9e6f64edd6be7ac4fb02199d943fb0d7c380b
noleynik29 <noleynik29@gmail.com>
2026-02-18 14:58:24 +0200

    spring boot category

--- src/main/java/com/example/springboot/entity/Category.java
+++ src/main/java/com/example/springboot/entity/Category.java
@@ -5,21 +5,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
+@Entity
+@Table(name = "categories")
 @Getter
 @Setter
-@Entity
-@Table(name = "category")
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     private String description;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @ManyToMany(mappedBy = "categories")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Book> books = new HashSet<>();
 }
