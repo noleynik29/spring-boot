228a571c4fdfb2c239b37acb37162023b71d8b3d
noleynik29 <noleynik29@gmail.com>
2026-02-25 00:17:33 +0200

    spring boot shopping cart

--- src/main/java/com/example/springboot/repository/role/RoleRepository.java
+++ src/main/java/com/example/springboot/repository/role/RoleRepository.java
@@ -1,7 +1,6 @@
 package com.example.springboot.repository.role;
 
 import com.example.springboot.entity.Role;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
