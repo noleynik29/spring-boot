a5774ba83af09fa2688d1111366e70dba67a3a37
noleynik29 <noleynik29@gmail.com>
2026-02-26 01:06:57 +0200

    spring boot order

--- src/main/java/com/example/springboot/controller/OrderController.java
+++ src/main/java/com/example/springboot/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.example.springboot.dto.order.CreateOrderRequestDto;
 import com.example.springboot.dto.order.OrderDto;
 import com.example.springboot.dto.order.OrderItemDto;
 import com.example.springboot.dto.order.UpdateOrderStatusRequestDto;
-import com.example.springboot.entity.Order;
 import com.example.springboot.service.order.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
